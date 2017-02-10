8ee9907f66bac955f10fd487a87a577f94816c84
Brian Mullen <bmullen@molbie.co>
2017-02-10 02:07:29 -0700

    add support for swift package manager

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Flexii
+//
+//  Created by Brian Mullen on 2/8/17.
+//  Copyright © 2017 Molbie LLC. All rights reserved.
+//
+
+import PackageDescription
+
+
+let package = Package(
+  name: "Flexii"
+)
